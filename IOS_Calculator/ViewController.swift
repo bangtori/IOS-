82e748ca5f8991bbd4fedbd0f678b04fe908309f
Tori <ttang1135@naver.com>
2023-03-23 00:41:03 +0900

    아울렛 연결

--- IOS_Calculator/ViewController.swift
+++ IOS_Calculator/ViewController.swift
@@ -8,7 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayLabel:UILabel!
+    @IBOutlet weak var btn0:UIButton
+    @IBOutlet weak var btn1:UIButton!
+    @IBOutlet weak var btn2:UIButton!
+    @IBOutlet weak var btn3:UIButton!
+    @IBOutlet weak var btn4:UIButton!
+    @IBOutlet weak var btn5:UIButton!
+    @IBOutlet weak var btn6:UIButton!
+    @IBOutlet weak var btn7:UIButton!
+    @IBOutlet weak var btn8:UIButton!
+    @IBOutlet weak var btn9:UIButton!
+    @IBOutlet weak var dotBtn:UIButton!
+    @IBOutlet weak var equalBtn:UIButton!
+    @IBOutlet weak var plusBtn:UIButton!
+    @IBOutlet weak var minusBtn:UIButton!
+    @IBOutlet weak var multiplyBtn:UIButton!
+    @IBOutlet weak var divideBtn:UIButton!
+    @IBOutlet weak var percentBtn:UIButton!
+    @IBOutlet weak var plusMinusBtn:UIButton!
+    @IBOutlet weak var clearBtn:UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
